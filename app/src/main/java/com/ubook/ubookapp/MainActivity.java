f832974e0c1bb317be5ce3f1145d268a08a8298e
uytai <uy.tai@edge-works.net>
2019-04-26 11:20:43 +0700

    Display mapview and show current location

--- app/src/main/java/com/ubook/ubookapp/MainActivity.java
+++ app/src/main/java/com/ubook/ubookapp/MainActivity.java
@@ -1,18 +1,39 @@
 package com.ubook.ubookapp;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.ubook.ubookapp.account.AccountFragment;
 import com.ubook.ubookapp.notify.NotifyFragment;
 import com.ubook.ubookapp.home.HomeFragment;
 import com.ubook.ubookapp.orders.OrdersFragment;
 
+import static com.ubook.ubookapp.Constants.ERROR_DIALOG_REQUEST;
+import static com.ubook.ubookapp.Constants.PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION;
+import static com.ubook.ubookapp.Constants.PERMISSIONS_REQUEST_ENABLE_GPS;
+
 public class MainActivity extends AppCompatActivity {
+
+    private boolean mLocationPermissionGranted = false;
+    private static final String TAG = "uytai";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +46,24 @@ public class MainActivity extends AppCompatActivity {
         // attaching bottom sheet behaviour - hide / show on scroll
         CoordinatorLayout.LayoutParams layoutParams = (CoordinatorLayout.LayoutParams) navigation.getLayoutParams();
         layoutParams.setBehavior(new BottomNavigationBehavior());
+        Fragment fragment;
+        fragment = new HomeFragment();
+        loadFragment(fragment);
+    }
+
+    //
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(checkMapServices()){
+            if(mLocationPermissionGranted){
+                //
+            }else{
+                getLocationPermission();
+            }
+        }
     }
 
     //
@@ -59,6 +98,113 @@ public class MainActivity extends AppCompatActivity {
         transaction.replace(R.id.flContainer, fragment);
         transaction.addToBackStack(null);
         transaction.commit();
+    }
+
+
+    //-------------------------------------------------------------------------------------------//
+    private boolean checkMapServices(){
+        if(isServicesOK()){
+            if(isMapsEnabled()){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void buildAlertMessageNoGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("This application requires GPS to work properly, do you want to enable it?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        Intent enableGpsIntent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivityForResult(enableGpsIntent, PERMISSIONS_REQUEST_ENABLE_GPS);
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    public boolean isMapsEnabled(){
+        final LocationManager manager = (LocationManager) getSystemService( Context.LOCATION_SERVICE );
+
+        if ( !manager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
+            buildAlertMessageNoGps();
+            return false;
+        }
+        return true;
+    }
+
+    private void getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true;
+            //getChatrooms();
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+    public boolean isServicesOK(){
+        Log.d(TAG, "isServicesOK: checking google services version");
+
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+
+        if(available == ConnectionResult.SUCCESS){
+            //everything is fine and the user can make map requests
+            Log.d(TAG, "isServicesOK: Google Play Services is working");
+            return true;
+        }
+        else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            //an error occured but we can resolve it
+            Log.d(TAG, "isServicesOK: an error occured but we can fix it");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }else{
+            Toast.makeText(this, "You can't make map requests", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        mLocationPermissionGranted = false;
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mLocationPermissionGranted = true;
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, "onActivityResult: called.");
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ENABLE_GPS: {
+                if(mLocationPermissionGranted){
+                    //getChatrooms();
+                }
+                else{
+                    getLocationPermission();
+                }
+            }
+        }
+
     }
 
 }
--- app/src/main/java/com/ubook/ubookapp/home/HomeFragment.java
+++ app/src/main/java/com/ubook/ubookapp/home/HomeFragment.java
@@ -1,15 +1,31 @@
 package com.ubook.ubookapp.home;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.ubook.ubookapp.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
-public class HomeFragment extends Fragment {
+import static com.ubook.ubookapp.Constants.MAPVIEW_BUNDLE_KEY;
+
+
+public class HomeFragment extends Fragment implements OnMapReadyCallback {
+
+    @BindView(R.id.mvUbook)
+    MapView mvUbook;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -19,6 +35,59 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        ButterKnife.bind(this, view);
+        initMapView(savedInstanceState);
+        return view;
+    }
+
+    //setup mapview
+    private void initMapView(Bundle savedInstanceState) {
+        Bundle mapviewBundle = null;
+        if (savedInstanceState != null) {
+            mapviewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
+        }
+        mvUbook.onCreate(mapviewBundle);
+        mvUbook.getMapAsync(this);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mvUbook.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mvUbook.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mvUbook.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mvUbook.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mvUbook.onDestroy();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(0, 1)).title("Marker"));
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        googleMap.setMyLocationEnabled(true);
     }
 }
